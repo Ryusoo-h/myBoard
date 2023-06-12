5eeb0b8aa550f755089cccd7467decb08df2fb43
soohyeon <su132489@gmail.com>
2023-06-12 17:34:59 +0900

    Feat, Design: Study List UI 변경

--- script/page-home/createStudyTable.js
+++ script/page-home/createStudyTable.js
@@ -3,8 +3,9 @@ import { calculateDday } from "../util/calculateDday.js";
 // Study List의 표 계산해주는 script
 
 class CreateStudyTable {
-    constructor(data) {
+    constructor(data, isOpen) {
         this.data = data;
+        this.isOpen = isOpen; // true | false
     }
 
     // 해당 타입에 맞는 단위 출력
@@ -59,28 +60,46 @@ class CreateStudyTable {
     render() {
         const Card = document.querySelector(`#studyList #${this.data.id}`);
 
-        const { Dday, type, totalPage, currentPage } = this.data.information;
+        const { startDay, Dday, type, totalAmount, currentAmount } = this.data.information;
         const unit = this.getTypeUnit(type);
         const calculatedDday = calculateDday(Dday);
         let DdayNum = Number(calculatedDday.split('D')[1]);
         DdayNum = calculatedDday === 'D-day' ? 0 
         : DdayNum !== undefined ? DdayNum : 1;
 
-        // Dday 출력
-        Card.querySelector('.Dday').innerHTML= this.printDate(Dday);
-        // totalPage 출력
-        Card.querySelector('.totalPage').innerHTML= totalPage + unit;
+        const progressPercent = Math.round((currentAmount / totalAmount * 100));
+        // main 출력
+        const mainTemplate = `
+            <div class="main">
+                <div class="title-and-d-day flex">
+                    <h2>${this.data.title}</h2>
+                    <div class="d-day-and-progress flex">
+                        <span class="d-day">${this.data.complete ? `목표달성🎉 (완료일 : D${this.data.information.table[0].Dday})` : calculatedDday}</span>
+                        <progress value="${progressPercent}" min="0" max="100"></progress>
+                        <span class="percent">${progressPercent}%</span>
+                    </div>
+                </div>
+                <div class="detail flex">
+                    <p class="memo">
+                        ${this.data.memo} ${type + ': ' + totalAmount + unit}
+                    </p>
+                    <p class="date"> ${DdayNum > 0 ? `${startDay} ~ ${Dday}` : `시작일 ${startDay}`}</p>
+                </div>
+            </div>
+        `;
+        Card.insertAdjacentHTML('afterbegin', mainTemplate);
+        if (this.data.complete) {
+            Card.classList.add('complete');
+        } else if (DdayNum > 0) {
+            Card.classList.add('incomplete');
+        }
+
         // table 출력을 위한 데이터 처리
         const tableTemplate = `
-            <table>
-                <thead>
-                    ${totalPage === (currentPage || 0) ? `
-                            <tr>
-                                <th colspan="4" style="border-right: none; background-color: var(--point); color: var(--bg-content)" class="date">
-                                    목표달성🎉 (완료일 : D${this.data.information.table[0].Dday})
-                                </th>
-                            </tr>
-                        ` : `
+            <div class="amount-table ${this.isOpen ? '' : 'hidden'}">
+                <table>
+                    <thead>
+                        ${totalAmount === (currentAmount || 0) ? '' : `
                             ${DdayNum > 0 ? `
                                 <tr>
                                     <th colspan="4" style="border-right: none; background-color: var(--red); color: var(--bg-content)" class="date">
@@ -94,77 +113,140 @@ class CreateStudyTable {
                                     </th>
                                 </tr>
                             `}
-                        `
-                    }
-                    
-                    <tr>
-                        <th class="date">날짜</th>
-                        <th width="18%" class="page">진행된<br> ${type} 수</th>
-                        <th width="18%" class="progress">총 진행률<br>(%)</th>
-                        <th class="quota">목표까지 1일 최소 할당량<br>(남은 ${type} 수 / 남은 일수)</th>
-                    </tr>
-                </thead>
-                <tbody></tbody>
-            </table>
+                        `}
+                        <tr>
+                            <th width="20%" class="date">날짜</th>
+                            <th width="35%" class="amount">총 ${type} 수</th>
+                            <th width="20%" class="d-day">D-day</th>
+                            <th width="25%" class="quota">총 진행률(%)</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
         `;
-        let quotaPage = 0;
+        let quotaAmount = 0;
         const trList = this.data.information.table?.map((item, index) => {
-                const progress = ((item.page / totalPage)*100).toFixed(2);
-                const remainingPages = totalPage - item.page;
-                let quota = Math.ceil(remainingPages / Math.abs(item.Dday < 0 ? item.Dday : 1));
+                const progress = ((item.amount / totalAmount)*100).toFixed(2);
+                const remainingAmount = totalAmount - item.amount;
+                let quota = Math.ceil(remainingAmount / Math.abs(item.Dday < 0 ? item.Dday : 1));
                 if(index === 0) { // 가장 최근 날짜일 경우 할당량을 구함
-                    quotaPage = item.page + quota;
+                    quotaAmount = item.amount + quota;
                 }
                 return `
                         <tr>
                             <td class="date">${item.date}</td>
-                            <td class="page">${item.page + unit}</td>
+                            <td class="amount">${item.amount + unit}</td>
+                            <td class="quota">D${item.Dday}</td>
                             <td class="progress">${progress}%</td>
-                            <td class="quota">${quota + unit} (${remainingPages}/ D${item.Dday})</td>
                         </tr>
                     `
             }).join('')
             || '';
+            
+            // <th class="date">날짜</th>
+            // <th width="18%" class="amount">총 ${type} 수</th>
+            // <th width="25%" class="day-amount">하루 ${type} 수</th>
+            // <th class="d-day">D-day</th>
+            // <th width="18%" class="progress">총 진행률(%)</th>
         const today = new Date();
-        const recentPage = this.data.information.table[0]?.page || 0;
-        let trMemo = `
-                <tr style="background-color: var(--bg-basic);">
-                    <td colspan=4 style="text-align:center; ">목표 달성!</td>
-                </tr>
-            `;
-        if (totalPage !== recentPage) {
-            let quota = Math.ceil((totalPage - recentPage) / Math.abs(DdayNum));
-            quotaPage = recentPage + quota;
-            const remainingQuota = quotaPage - (currentPage || 0);
-            trMemo = `
-                <tr style="background-color: var(--bg-basic);">
-                    <td style="border-right: none;" class="date">${this.printDate(today)}</td>
-                    <td style="border-right: none;" class="page">${currentPage + unit}</td>
-                    <td style="border-right: none;" class="progress"></td>
-                    <td class="quota">할당량 : ${quotaPage + unit}까지 ${remainingQuota + unit}남음</td>
-                </tr>
-            `;
-        }
+        const recentAmount = this.data.information.table[0]?.amount || 0;
 
         // table 출력
-        Card.querySelector('details').insertAdjacentHTML('beforeend',tableTemplate);
-        Card.querySelector('details table tbody').innerHTML = trList;
-        Card.querySelector('details table tbody').insertAdjacentHTML('afterbegin',trMemo);
+        Card.insertAdjacentHTML('beforeend',tableTemplate);
+        Card.querySelector('table tbody').innerHTML = trList;
+
+        // table 상단에 할당량 출력
+        if (totalAmount !== recentAmount) {
+            let quota = Math.ceil((totalAmount - recentAmount) / Math.abs(DdayNum));
+            quotaAmount = recentAmount + quota;
+            const remainingQuota = quotaAmount - (currentAmount || 0);
+            let trMemo = `
+                <tr style="background-color: var(--bg-basic);">
+                    <td style="border-right: none;" class="date">${this.printDate(today)}</td>
+                    <td style="border-right: none;" class="amount">${currentAmount + unit}</td>
+                    <td colspan="2" class="quota">할당량 : ${quotaAmount + unit}까지 ${remainingQuota + unit}남음</td>
+                </tr>
+            `;
+            Card.querySelector('table thead tr:first-child').insertAdjacentHTML('afterend',trMemo);
+        }
+        Card.querySelector('.main').addEventListener('click', function() {
+            this.parentNode.querySelector('.amount-table').classList.toggle('hidden');
+        })
     }
-    
 }
+
+// TEST
+// const test = new CreateStudyTable({
+//     id : 'test',
+//     title : 'TEST',
+//     memo : '테스트 입니다!,',
+//     complete: false,
+//     information : {
+//         startDay : '2023-06-12',
+//         Dday : '2023-06-14',
+//         type : '문제',
+//         totalAmount : 10,
+//         currentAmount: 2,
+//         table : [
+//             { date : '6/12(월)', amount: 2, Dday: '-2'}
+//         ],
+//     }
+// });
+// test.render();
+
+// 리액트를 다루는 기술
+const reactVelopert = new CreateStudyTable({
+    id : 'react-velopert',
+    title : '리액트를 다루는 기술',
+    memo : '드디어 프로젝트!',
+    complete: false,
+    information : {
+        startDay : '2022-12-14',
+        Dday : '2023-06-18',
+        type : '페이지',
+        totalAmount : 905,
+        currentAmount: 776,
+        table : [
+            { date : '5/19(금)', amount: 776, Dday: '+5'},
+            { date : '5/12(금)', amount: 770, Dday: '-2'},
+            { date : '5/11(목)', amount: 763, Dday: '-3'},
+            { date : '5/8(월)', amount: 752, Dday: '-6'},
+            { date : '5/7(일)', amount: 714, Dday: '-7'},
+            { date : '1/3(화)', amount: 468, Dday: '-5'},
+            { date : '1/2(월)', amount: 442, Dday: '-6'},
+            { date : '12/30(금)', amount: 412, Dday: '-6'},
+            { date : '12/29(목)', amount: 381, Dday: '-9'},
+            { date : '12/28(수)', amount: 355, Dday: '-10'},
+            { date : '12/27(화)', amount: 317, Dday: '-11'},
+            { date : '12/26(월)', amount: 304, Dday: '-12'},
+            { date : '12/22(목)', amount: 250, Dday: '-13'},
+            { date : '12/19(월)', amount: 180, Dday: '-17'},
+            { date : '12/18(일)', amount: 162, Dday: '-21'},
+            { date : '12/15(목)', amount: 92, Dday: '-23'},
+            { date : '12/14(수)', amount: 69, Dday: '-24'}
+        ],
+    }
+}, true);
+reactVelopert.render();
 
 // SQLD
 const sqld = new CreateStudyTable({
     id : 'sqld',
+    title : 'SQLD',
+    memo : '6/10(토) 시험응시! <br>복습 : <a href="/word-memorization/?post=post1">단어 암기장</a>,',
+    complete: false,
     information : {
+        startDay : '2023-06-04',
         Dday : '2023-06-10',
         type : '문제',
-        totalPage : 146,
-        currentPage: 50,
+        totalAmount : 146,
+        currentAmount: 90,
         table : [
-            { date : '6/5(월)', page: 50, Dday: '-5'},
-            { date : '6/4(일)', page: 30, Dday: '-6'}
+            { date : '6/9(금)', amount: 90, Dday: '-day'},
+            { date : '6/7(수)', amount: 70, Dday: '-3'},
+            { date : '6/5(월)', amount: 50, Dday: '-5'},
+            { date : '6/4(일)', amount: 30, Dday: '-6'}
         ],
     }
 });
@@ -173,65 +255,39 @@ sqld.render();
 // 정보처리기사 필기
 const engineerInformationProcessing = new CreateStudyTable({
     id : 'engineer-information-processing',
+    title : '정보처리기사 필기',
+    memo : '6/1(목) 시험 합격',
+    complete: true,
     information : {
+        startDay : '2023-05-19',
         Dday : '2023-06-01',
         type : '페이지',
-        totalPage : 888,
-        currentPage: 888,
+        totalAmount : 888,
+        currentAmount: 888,
         table : [
-            { date : '5/28(수)', page: 888, Dday: '-1'},
-            { date : '5/28(화)', page: 704, Dday: '-2'},
-            { date : '5/28(월)', page: 606, Dday: '-3'},
-            { date : '5/28(일)', page: 531, Dday: '-4'},
-            { date : '5/27(토)', page: 466, Dday: '-5'},
-            { date : '5/25(목)', page: 414, Dday: '-7'},
-            { date : '5/24(수)', page: 346, Dday: '-8'},
-            { date : '5/23(화)', page: 303, Dday: '-9'},
-            { date : '5/22(월)', page: 264, Dday: '-10'},
-            { date : '5/21(일)', page: 256, Dday: '-11'},
-            { date : '5/20(토)', page: 236, Dday: '-12'},
-            { date : '5/19(금)', page: 145, Dday: '-13'},
-            // { date : '1/3(화)', page: 303, Dday: 5},
-            // { date : '1/2(월)', page: 270, Dday: 6},
-            // { date : '12/29(목)', page: 233, Dday: 6},
-            // { date : '12/28(수)', page: 184, Dday: 10},
-            // { date : '12/27(화)', page: 175, Dday: 11},
-            // { date : '12/19(월)', page: 148, Dday: 13},
-            // { date : '~11/18(일)', page: 145, Dday: 20},
+            { date : '5/28(수)', amount: 888, Dday: '-1'},
+            { date : '5/28(화)', amount: 704, Dday: '-2'},
+            { date : '5/28(월)', amount: 606, Dday: '-3'},
+            { date : '5/28(일)', amount: 531, Dday: '-4'},
+            { date : '5/27(토)', amount: 466, Dday: '-5'},
+            { date : '5/25(목)', amount: 414, Dday: '-7'},
+            { date : '5/24(수)', amount: 346, Dday: '-8'},
+            { date : '5/23(화)', amount: 303, Dday: '-9'},
+            { date : '5/22(월)', amount: 264, Dday: '-10'},
+            { date : '5/21(일)', amount: 256, Dday: '-11'},
+            { date : '5/20(토)', amount: 236, Dday: '-12'},
+            { date : '5/19(금)', amount: 145, Dday: '-13'},
+            // { date : '1/3(화)', amount: 303, Dday: 5},
+            // { date : '1/2(월)', amount: 270, Dday: 6},
+            // { date : '12/29(목)', amount: 233, Dday: 6},
+            // { date : '12/28(수)', amount: 184, Dday: 10},
+            // { date : '12/27(화)', amount: 175, Dday: 11},
+            // { date : '12/19(월)', amount: 148, Dday: 13},
+            // { date : '~11/18(일)', amount: 145, Dday: 20},
         ],
     }
 });
 engineerInformationProcessing.render();
 
-// 리액트를 다루는 기술
-const reactVelopert = new CreateStudyTable({
-    id : 'react-velopert',
-    information : {
-        Dday : '2023-05-14',
-        type : '페이지',
-        totalPage : 905,
-        currentPage: 776,
-        table : [
-            { date : '5/19(금)', page: 776, Dday: '+5'},
-            { date : '5/12(금)', page: 770, Dday: '-2'},
-            { date : '5/11(목)', page: 763, Dday: '-3'},
-            { date : '5/8(월)', page: 752, Dday: '-6'},
-            { date : '5/7(일)', page: 714, Dday: '-7'},
-            { date : '1/3(화)', page: 468, Dday: '-5'},
-            { date : '1/2(월)', page: 442, Dday: '-6'},
-            { date : '12/30(금)', page: 412, Dday: '-6'},
-            { date : '12/29(목)', page: 381, Dday: '-9'},
-            { date : '12/28(수)', page: 355, Dday: '-10'},
-            { date : '12/27(화)', page: 317, Dday: '-11'},
-            { date : '12/26(월)', page: 304, Dday: '-12'},
-            { date : '12/22(목)', page: 250, Dday: '-13'},
-            { date : '12/19(월)', page: 180, Dday: '-17'},
-            { date : '12/18(일)', page: 162, Dday: '-21'},
-            { date : '12/15(목)', page: 92, Dday: '-23'},
-            { date : '12/14(수)', page: 69, Dday: '-24'}
-        ],
-    }
-});
-reactVelopert.render();
 
 
